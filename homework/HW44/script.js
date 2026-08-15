8b0ced06ef1ca1424ea167b5492e96b0fc0708f2
Kristina <kristinamiroyan9@gmail.com>
2026-08-15 14:02:32 +0300

    homework44

--- homework/HW44/script.js
+++ homework/HW44/script.js
@@ -1,5 +1,5 @@
 let note = document.querySelector('#note');
 note.value = localStorage.getItem('note');
 note.addEventListener('input', function () { //сохраняет значение при каждом изменении
-    localStorage.setItem('note', note.value);
-});
+    localStorage.setItem('note', note.value); 
+}); 
